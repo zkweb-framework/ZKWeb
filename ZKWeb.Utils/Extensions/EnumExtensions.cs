7f1ec2ed51cd4db37cbe5c15847c1e9637b0ecbe
303248153 <303248153@qq.com>
2015-12-31 10:57:56 +0800

    修改GetDisplayName到GetDescription，不再支持从Display属性获取名称

--- ZKWeb.Utils/Extensions/EnumExtensions.cs
+++ ZKWeb.Utils/Extensions/EnumExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,12 @@ namespace ZKWeb.Utils.Extensions {
 	/// </summary>
 	public static class EnumExtensions {
 		/// <summary>
-		/// 获取枚举值的显示名称
-		/// 有指定Display属性时返回该属性的对应名称，否则返回字段本身的名称
+		/// 获取枚举值的描述
+		/// 有指定Description属性时返回该属性的对应名称，否则返回字段本身的名称
 		/// </summary>
 		/// <param name="value">枚举值</param>
 		/// <returns></returns>
-		public static string GetDisplayName(this Enum value) {
+		public static string GetDescription(this Enum value) {
 			// 获取枚举值类型和名称
 			Type type = value.GetType();
 			string name = Enum.GetName(type, value);
@@ -25,10 +26,10 @@ namespace ZKWeb.Utils.Extensions {
 				// 值不在类型中时返回数字
 				return Convert.ToInt32(value).ToString();
 			}
-			// 获取Display属性
-			var displayAttribute = value.GetAttribute<DisplayAttribute>();
-			if (displayAttribute != null) {
-				return displayAttribute.GetName() ?? displayAttribute.GetShortName();
+			// 获取Description属性
+			var attribute = value.GetAttribute<DescriptionAttribute>();
+			if (attribute != null) {
+				return attribute.Description;
 			}
 			// 返回默认名称
 			return name;
