acf4dfd6c38e90dfc94402755fd4fa639c06d993
303248153 <303248153@qq.com>
2016-01-03 14:13:10 +0800

    支持描画T到模板

--- ZKWeb/Core/T.cs
+++ ZKWeb/Core/T.cs
@@ -1,4 +1,5 @@
-﻿using DryIoc;
+﻿using DotLiquid;
+using DryIoc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace ZKWeb.Core {
 	/// 实际翻译会在转换到字符串时执行，使用T类型保存文本可以用于延迟翻译
 	/// 这个类可以直接转换到string，也可以使用ToString转换
 	/// </summary>
-	public struct T {
+	public struct T : ILiquidizable {
 		/// <summary>
 		/// 翻译前的文本
 		/// </summary>
@@ -34,6 +35,14 @@ namespace ZKWeb.Core {
 		/// <param name="t"></param>
 		public static implicit operator string(T t) {
 			return t.ToString();
+		}
+
+		/// <summary>
+		/// 允许描画到模板
+		/// </summary>
+		/// <returns></returns>
+		object ILiquidizable.ToLiquid() {
+			return ToString();
 		}
 
 		/// <summary>
