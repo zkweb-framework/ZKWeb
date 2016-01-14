69d2fa99ff14f51deca6aac4d4ed9ff0aa47a940
303248153 <303248153@qq.com>
2016-01-14 14:04:43 +0800

    移动AddAttributes函数

---
+++ ZKWeb.Utils/Extensions/HtmlTextWriterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace ZKWeb.Utils.Extensions {
+	/// <summary>
+	/// Html构建器的扩展函数
+	/// </summary>
+	public static class HtmlTextWriterExtensions {
+		/// <summary>
+		/// 批量添加元素的属性
+		/// </summary>
+		/// <param name="html">html构建器</param>
+		/// <param name="attributes">元素属性的集合</param>
+		public static void AddAttributes(
+			this HtmlTextWriter html, IEnumerable<KeyValuePair<string, string>> attributes) {
+			foreach (var attr in attributes) {
+				html.AddAttribute(attr.Key, attr.Value);
+			}
+		}
+	}
+}
