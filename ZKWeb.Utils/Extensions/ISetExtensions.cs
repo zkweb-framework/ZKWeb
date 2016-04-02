1b3ef565d92e26aee19934b551f483f2377314ce
303248153 <303248153@qq.com>
2016-04-02 14:00:08 +0800

    添加ISet.AddRange

---
+++ ZKWeb.Utils/Extensions/ISetExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZKWeb.Utils.Extensions {
+	/// <summary>
+	/// 集合的扩展函数
+	/// </summary>
+	public static class ISetExtensions {
+		/// <summary>
+		/// 添加元素列表到集合中
+		/// 返回添加成功的数量
+		/// </summary>
+		/// <typeparam name="T">元素类型</typeparam>
+		/// <param name="set">集合</param>
+		/// <param name="items">元素列表</param>
+		/// <returns></returns>
+		public static long AddRange<T>(this ISet<T> set, IEnumerable<T> items) {
+			long result = 0;
+			foreach (var item in items) {
+				result += set.Add(item) ? 1 : 0;
+			}
+			return result;
+		}
+	}
+}
