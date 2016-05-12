3ef4525edf8d4820d09597beb0fa9ce46e463b66
303248153 <303248153@qq.com>
2016-05-12 10:42:49 +0800

    改进MemoryCache的性能

--- ZKWeb.Utils/Collections/MemoryCache.cs
+++ ZKWeb.Utils/Collections/MemoryCache.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ZKWeb.Utils.Extensions;
 using ZKWeb.Utils.Functions;
@@ -11,13 +12,13 @@ namespace ZKWeb.Utils.Collections {
 	/// <summary>
 	/// 缓存类，支持指定过期时间，线程安全
 	/// </summary>
-	/// <typeparam name="TKey"></typeparam>
-	/// <typeparam name="TValue"></typeparam>
+	/// <typeparam name="TKey">键类型</typeparam>
+	/// <typeparam name="TValue">值类型</typeparam>
 	public class MemoryCache<TKey, TValue> {
 		/// <summary>
 		/// 定期删除过期数据的间隔时间，秒
 		/// </summary>
-		public const int RevokeExpiresInteval = 180;
+		public const int RevokeExpiresInterval = 180;
 		/// <summary>
 		/// 缓存数据
 		/// 结构 { 键, (对象, 过期时间) }
@@ -28,7 +29,8 @@ namespace ZKWeb.Utils.Collections {
 		/// <summary>
 		/// 缓存数据的线程锁
 		/// </summary>
-		internal object CacheLock { get; set; } = new object();
+		internal ReaderWriterLockSlim CacheLock { get; set; } =
+			new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
 		/// <summary>
 		/// 最后一次删除过期缓存的时间
 		/// </summary>
@@ -39,10 +41,13 @@ namespace ZKWeb.Utils.Collections {
 		/// 固定每180秒一次
 		/// </summary>
 		void RevokeExpires() {
-			lock (CacheLock) {
-				var now = DateTime.UtcNow;
-				if ((now - LastRevokeExpires).TotalSeconds < RevokeExpiresInteval) {
-					return;
+			var now = DateTime.UtcNow;
+			if ((now - LastRevokeExpires).TotalSeconds < RevokeExpiresInterval) {
+				return;
+			}
+			using (CacheLock.WithWriteLock()) {
+				if ((now - LastRevokeExpires).TotalSeconds < RevokeExpiresInterval) {
+					return; // double check
 				}
 				LastRevokeExpires = now;
 				var expireKeys = Cache.Where(c => c.Value.Item2 < now).Select(c => c.Key).ToList();
@@ -60,7 +65,7 @@ namespace ZKWeb.Utils.Collections {
 		/// <param name="keepTime">保留时间</param>
 		public void Put(TKey key, TValue value, TimeSpan keepTime) {
 			RevokeExpires();
-			lock (CacheLock) {
+			using (CacheLock.WithWriteLock()) {
 				var now = DateTime.UtcNow;
 				Cache[key] = Tuple.Create(value, now + keepTime);
 			}
@@ -75,7 +80,7 @@ namespace ZKWeb.Utils.Collections {
 		/// <returns></returns>
 		public TValue GetOrDefault(TKey key, TValue defaultValue = default(TValue)) {
 			RevokeExpires();
-			lock (CacheLock) {
+			using (CacheLock.WithReadLock()) {
 				var now = DateTime.UtcNow;
 				var value = Cache.GetOrDefault(key);
 				if (value != null && value.Item2 > now) {
@@ -91,7 +96,7 @@ namespace ZKWeb.Utils.Collections {
 		/// <param name="key">缓存键</param>
 		public void Remove(TKey key) {
 			RevokeExpires();
-			lock (CacheLock) {
+			using (CacheLock.WithWriteLock()) {
 				Cache.Remove(key);
 			}
 		}
@@ -100,7 +105,7 @@ namespace ZKWeb.Utils.Collections {
 		/// 清空缓存数据
 		/// </summary>
 		public void Clear() {
-			lock (CacheLock) {
+			using (CacheLock.WithWriteLock()) {
 				Cache.Clear();
 			}
 		}
---
+++ ZKWeb.Utils/Tests/Extensions/ReaderWriterLockSlimExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ZKWeb.Utils.Extensions;
+using ZKWeb.Utils.UnitTest;
+
+namespace ZKWeb.Utils.Tests.Extensions {
+	[UnitTest]
+	class ReaderWriterLockSlimExtensionsTest {
+		public void WithReadLock() {
+			var rwlock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+			using (rwlock.WithReadLock()) {
+				Assert.Throws<LockRecursionException>(() => rwlock.TryEnterReadLock(1));
+			}
+			Assert.IsTrue(rwlock.TryEnterReadLock(1));
+			rwlock.ExitReadLock();
+		}
+
+		public void WithWriteLock() {
+			var rwlock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+			using (rwlock.WithWriteLock()) {
+				Assert.Throws<LockRecursionException>(() => rwlock.TryEnterWriteLock(1));
+			}
+			Assert.IsTrue(rwlock.TryEnterWriteLock(1));
+			rwlock.ExitWriteLock();
+		}
+	}
+}
