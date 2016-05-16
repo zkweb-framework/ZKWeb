b3d947086bc0ed56928ce9e57ceee46a53547957
303248153 <303248153@qq.com>
2016-05-16 13:41:34 +0800

    修改FileUtils.GetSizeDisplayName的参数类型到long

--- ZKWeb.Utils/Functions/FileUtils.cs
+++ ZKWeb.Utils/Functions/FileUtils.cs
@@ -13,9 +13,9 @@ namespace ZKWeb.Utils.Functions {
 		/// 获取字节大小的显示名称
 		/// 自动转换成KB、MB或GB格式
 		/// </summary>
-		/// <param name="bytes"></param>
+		/// <param name="bytes">字节数</param>
 		/// <returns></returns>
-		public static string GetSizeDisplayName(int bytes) {
+		public static string GetSizeDisplayName(long bytes) {
 			if (bytes >= 1073741824) {
 				return $"{Decimal.Round((decimal)bytes / 1073741824, 2)}GB";
 			} else if (bytes >= 1048576) {
