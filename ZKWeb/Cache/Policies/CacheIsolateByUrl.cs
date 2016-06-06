857857ed47d4ff249394600776d1964a86bf5204
303248153 <303248153@qq.com>
2016-06-06 11:44:05 +0800

    补充遗漏的接口

--- ZKWeb/Cache/Policies/CacheIsolateByUrl.cs
+++ ZKWeb/Cache/Policies/CacheIsolateByUrl.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ZKWeb.Cache.Interfaces;
 using ZKWeb.Utils.Functions;
 
 namespace ZKWeb.Cache.Policies {
 	/// <summary>
 	/// 按当前Url和请求参数隔离缓存
 	/// </summary>
-	public class CacheIsolateByUrl {
+	public class CacheIsolateByUrl : ICacheIsolationPolicy {
 		/// <summary>
 		/// 获取隔离键
 		/// </summary>
