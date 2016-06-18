0af9a41237874f5734a1521e8b3462e8230a080c
303248153 <303248153@qq.com>
2016-06-18 16:00:31 +0800

    URL隔离策略不应缓存路径

--- ZKWeb/Cache/Policies/CacheIsolateByUrl.cs
+++ ZKWeb/Cache/Policies/CacheIsolateByUrl.cs
@@ -9,25 +9,14 @@ namespace ZKWeb.Cache.Policies {
 	/// </summary>
 	public class CacheIsolateByUrl : ICacheIsolationPolicy {
 		/// <summary>
-		/// 缓存隔离键时使用的键名
-		/// 缓存在上下文时会忽略Fetch标签指定的Url并使用原始Url缓存
-		/// </summary>
-		public string UrlKey = "ZKWeb.CacheIsolateByUrlKey";
-
-		/// <summary>
 		/// 获取隔离键
 		/// </summary>
 		/// <returns></returns>
 		public object GetIsolationKey() {
-			// 从缓存获取
-			var context = HttpManager.CurrentContext;
-			var key = context.GetData<string>(UrlKey);
-			if (key != null) {
-				return key;
-			}
 			// 从路径和请求参数生成
+			var context = HttpManager.CurrentContext;
 			var request = context.Request;
-			key = request.Path;
+			var key = request.Path;
 			if (request.GetFormValues().Any()) {
 				// 有表单值时，不能只以Url参数隔离，需要以全部参数隔离
 				key += '?';
@@ -36,8 +25,6 @@ namespace ZKWeb.Cache.Policies {
 				// 没有表单值，但是有Url参数时，需要以Url参数隔离
 				key += request.QueryString;
 			}
-			// 保存到缓存
-			context.PutData(UrlKey, key);
 			return key;
 		}
 	}
