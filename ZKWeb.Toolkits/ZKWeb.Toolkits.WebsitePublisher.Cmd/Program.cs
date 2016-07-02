150f897924c9cafb3cb4d5d2b5b0b4a75f9e48a6
303248153 <303248153@qq.com>
2016-07-02 17:36:59 +0900

    添加命令行的网站发布器

--- ZKWeb.Toolkits/ZKWeb.Toolkits.WebsitePublisher.Cmd/Program.cs
+++ ZKWeb.Toolkits/ZKWeb.Toolkits.WebsitePublisher.Cmd/Program.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
+using System.IO;
 using ZKWeb.Toolkits.WebsitePublisher.Model;
 
 namespace ZKWeb.Toolkits.WebsitePublisher.Cmd {
 	public class Program {
 		public static void Main(string[] args) {
 			var parameters = new PublishWebsiteParameters();
-			parameters.WebRoot = @"D:\tmp\projects\Test.AspNet\Test.AspNet";
-			parameters.OutputName = "Test.AspNet";
-			parameters.OutputDirectory = @"D:\tmp\publish";
-			var publisher = new WebsitePublisher(parameters);
-			publisher.PublishWebsite();
-			Console.WriteLine("done");
+			var showHelp = false;
+			var optionSet = new OptionSet() {
+				{ "r|root=", "The source website root directory", r => parameters.WebRoot = r },
+				{ "n|name=", "The output name", n => parameters.OutputName = n },
+				{ "o|output=", "The output directory", d => parameters.OutputDirectory = d },
+				{ "h|help", "Show this message and exit", h => showHelp = (h != null) }
+			};
+			try {
+				optionSet.Parse(args);
+				if (showHelp) {
+					var optionDescriptions = new StringWriter();
+					optionSet.WriteOptionDescriptions(optionDescriptions);
+					Console.WriteLine("WebsitePublisher:");
+					Console.WriteLine(optionDescriptions.ToString());
+					return;
+				}
+				var publisher = new WebsitePublisher(parameters);
+				publisher.PublishWebsite();
+				Console.WriteLine("success");
+			} catch (Exception e) {
+				if (e is OptionException || e is ArgumentException) {
+					Console.WriteLine(e.Message);
+					Console.WriteLine("Try `WebsitePublisher --help` for more information");
+				} else {
+					Console.WriteLine(e.ToString());
+				}
+			}
 		}
 	}
 }
