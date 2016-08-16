c8868d04070a1e57b9f2db590e65acea0ac653d4
dudes-come <dudes@qq.com>
2016-08-16 17:12:13 +0800

    update asp.net core templates

---
+++ Tools/Templates/AspNetCore.EFCore/src/ProjectName.AspNetCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace ${ProjectName}.AspNetCore {
+	/// <summary>
+	/// Asp.Net Core Main Program
+	/// </summary>
+	public class Program {
+		/// <summary>
+		/// Entry Point
+		/// </summary>
+		/// <param name="args"></param>
+		public static void Main(string[] args) {
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.Build();
+			host.Run();
+		}
+	}
+}
---
+++ Tools/Templates/AspNetCore.MongoDB/src/ProjectName.AspNetCore/Startup.cs
@@ -0,0 +1,9 @@
+﻿using ZKWeb.Hosting.AspNetCore;
+
+namespace ${ProjectName}.AspNetCore {
+	/// <summary>
+	/// Asp.Net Core Startup Class
+	/// </summary>
+	public class Startup : StartupBase {
+	}
+}
