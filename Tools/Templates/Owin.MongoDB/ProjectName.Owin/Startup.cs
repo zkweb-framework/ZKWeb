d5a1b9de79eed01a35f0eaed5d79cea249f4b0f2
dudes-come <dudes@qq.com>
2016-08-18 14:07:23 +0800

    update owin templates

---
+++ Tools/Templates/Owin.MongoDB/ProjectName.Owin/Startup.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Owin;
+using ZKWeb.Hosting.Owin;
+
+[assembly: OwinStartup(typeof(${ProjectName}.Owin.Startup))]
+namespace ${ProjectName}.Owin {
+	/// <summary>
+	/// 程序入口点
+	/// </summary>
+	public class Startup : StartupBase { }
+}
