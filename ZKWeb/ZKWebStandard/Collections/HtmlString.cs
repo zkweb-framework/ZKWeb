e89d12a33e0d89a873e55fb420a6f43f8fc07101
dudes-come <dudes@qq.com>
2016-09-20 16:55:51 +0800

    添加HtmlString.Encode和HtmlString.Decode

--- ZKWeb/ZKWebStandard/Collections/HtmlString.cs
+++ ZKWeb/ZKWebStandard/Collections/HtmlString.cs
@@ -1,4 +1,6 @@
-﻿namespace ZKWebStandard.Collection {
+﻿using ZKWebStandard.Utils;
+
+namespace ZKWebStandard.Collection {
 	/// <summary>
 	/// Html string wrapper
 	/// </summary>
@@ -9,7 +11,7 @@
 		protected string Value { get; set; }
 
 		/// <summary>
-		/// Initialize
+		/// Initialize with html string, no encoding occurred
 		/// </summary>
 		/// <param name="value">Html string</param>
 		public HtmlString(string value) {
@@ -22,6 +24,23 @@
 		/// <returns></returns>
 		public override string ToString() {
 			return Value;
+		}
+
+		/// <summary>
+		/// Decode html string as text
+		/// </summary>
+		/// <returns></returns>
+		public string Decode() {
+			return HttpUtils.HtmlDecode(Value);
+		}
+
+		/// <summary>
+		/// Encode text as html string
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public static HtmlString Encode(string text) {
+			return new HtmlString(HttpUtils.HtmlEncode(text));
 		}
 	}
 }
---
+++ ZKWeb/ZKWebStandard/Tests/Collections/HtmlStringTest.cs
@@ -0,0 +1,15 @@
+﻿using ZKWebStandard.Collection;
+using ZKWebStandard.Testing;
+
+namespace ZKWebStandard.Tests.Collections {
+	[Tests]
+	class HtmlStringTest {
+		public void Encode() {
+			Assert.Equals(HtmlString.Encode("asd'\"<>").ToString(), "asd&#39;&quot;&lt;&gt;");
+		}
+
+		public void Decode() {
+			Assert.Equals(HtmlString.Encode("asd'\"<>").Decode(), "asd'\"<>");
+		}
+	}
+}
