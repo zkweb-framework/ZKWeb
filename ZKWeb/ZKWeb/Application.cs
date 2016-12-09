d481c70c2dc0cae984b852c75ab1960401ddbdbe
dudes-come <dudes@qq.com>
2016-12-09 15:42:13 +0800

    更新版本字符串

--- ZKWeb/ZKWeb/Application.cs
+++ ZKWeb/ZKWeb/Application.cs
@@ -31,7 +31,7 @@ namespace ZKWeb {
 		/// <summary>
 		/// ZKWeb Version String
 		/// </summary>
-		public static string FullVersion { get { return "1.3 beta 3"; } }
+		public static string FullVersion { get { return "1.3 final"; } }
 		/// <summary>
 		/// ZKWeb Version Object
 		/// </summary>
