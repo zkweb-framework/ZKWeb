10e5e0684afc62badeac345244bfa5084a430744
dudes-come <dudes@qq.com>
2017-04-25 15:35:30 +0800

    update version

--- ZKWeb/ZKWeb/Application.cs
+++ ZKWeb/ZKWeb/Application.cs
@@ -32,7 +32,7 @@ namespace ZKWeb {
 		/// <summary>
 		/// ZKWeb Version String
 		/// </summary>
-		public static string FullVersion { get { return "1.7.0 final"; } }
+		public static string FullVersion { get { return "1.8.0 beta 1"; } }
 		/// <summary>
 		/// ZKWeb Version Object
 		/// </summary>
