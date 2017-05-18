462a4a0254d42cdccb3a70c0c66bce0437a15d64
dudes-come <dudes@qq.com>
2017-05-18 09:45:52 +0800

    allow Asp.Net Core hosting take configuration from command line and json

--- ReleaseNotes/ReleaseNote.1.9.md
+++ ReleaseNotes/ReleaseNote.1.9.md
@@ -10,5 +10,7 @@
 	- Expression.ReplaceNode
 - Improve template engine
 	- Keep widget before and after html when render failed
+- Update project template
+	- Allow Asp.Net Core hosting take configuration from command line and json
 - Bug fixes
 	- Fix IFileEntry.WriteAllBytes
--- Tools/Templates/AspNetCore.EFCore/src/ProjectName.AspNetCore/Program.cs
+++ Tools/Templates/AspNetCore.EFCore/src/ProjectName.AspNetCore/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 
 namespace ${ProjectName}.AspNetCore {
 	/// <summary>
@@ -11,12 +13,22 @@ namespace ${ProjectName}.AspNetCore {
 		/// </summary>
 		/// <param name="args"></param>
 		public static void Main(string[] args) {
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseIISIntegration()
-				.UseStartup<Startup>()
-				.Build();
-			host.Run();
+			try {
+				var config = new ConfigurationBuilder()
+					.AddJsonFile("hosting.json", optional: true)
+					.AddCommandLine(args)
+					.Build();
+				var host = new WebHostBuilder()
+					.UseConfiguration(config)
+					.UseKestrel()
+					.UseIISIntegration()
+					.UseStartup<Startup>()
+					.Build();
+				host.Run();
+			} catch (Exception e) {
+				Console.WriteLine(e.ToString());
+				Environment.Exit(-1);
+			}
 		}
 	}
 }
