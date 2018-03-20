f6288e2aa4ec808bcbb2a7213297bf57bcd95cbb
dudes-come <dudes@qq.com>
2018-03-20 17:39:50 +0800

    support specific version from external file

--- ZKWeb/ZKWeb/Server/DefaultApplication.cs
+++ ZKWeb/ZKWeb/Server/DefaultApplication.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using ZKWeb.Cache;
 using ZKWeb.Cache.Policies;
@@ -37,12 +38,13 @@ namespace ZKWeb.Server {
 		/// ZKWeb Version String<br/>
 		/// ZKWeb的版本字符串<br/>
 		/// </summary>
-		public virtual string FullVersion { get { return "2.1.0"; } }
+		public virtual string FullVersion { get { return _fullVersion; } }
+		private string _fullVersion = "";
 		/// <summary>
 		/// ZKWeb Version Object<br/>
 		/// ZKWeb的版本对象<br/>
 		/// </summary>
-		public virtual Version Version { get { return Version.Parse(FullVersion.Split(' ')[0]); } }
+		public virtual Version Version { get { return Version.Parse(FullVersion.Split('-')[0]); } }
 		/// <summary>
 		/// The IoC Container Instance<br/>
 		/// IoC容器的实例<br/>
@@ -78,6 +80,16 @@ namespace ZKWeb.Server {
 		/// 网站根目录的路径<br/>
 		/// </summary>
 		protected string WebsiteRootDirectory { get; set; }
+
+		/// <summary>
+		/// Initialize<br/>
+		/// 初始化<br/>
+		/// </summary>
+		public DefaultApplication() {
+			_fullVersion = typeof(DefaultApplication).Assembly
+				.GetCustomAttributes(true)
+				.OfType<AssemblyInformationalVersionAttribute>().FirstOrDefault()?.InformationalVersion;
+		}
 
 		/// <summary>
 		/// Register components to the default container<br/>
