d5f365053dccbd4c87627640d8d9658ab761e44e
303248153 <303248153@qq.com>
2018-12-24 08:34:34 +0800

    update version to 2.2.0

--- Tools/Templates/build.bat
+++ Tools/Templates/build.bat
@@ -1,5 +1,8 @@
 @echo off
 
+set DevenvPath="D:\Program Files\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe"
+
+call :BuildNET AspNet.Dapper
 call :BuildNETCore AspNetCore.Dapper
 call :BuildNETCore AspNetCore.EFCore
 call :BuildNETCore AspNetCore.MongoDB
@@ -12,5 +15,12 @@ echo Build %~1
 cd %~1
 dotnet restore
 dotnet build -c Release
+cd ..
+exit /b 0
+
+:BuildNET
+echo Build %~1
+cd %~1
+%DevenvPath% /Build Release __ProjectName__.sln
 cd ..
 exit /b 0
--- Tools/Templates/clean.bat
+++ Tools/Templates/clean.bat
@@ -1,14 +1,35 @@
 @echo off
 
+call :CleanNET AspNet.Dapper
+call :CleanNET AspNet.EFCore
+call :CleanNET AspNet.MongoDB
+call :CleanNET AspNet.NHibernate
 call :CleanNETCore AspNetCore.Dapper
 call :CleanNETCore AspNetCore.EFCore
 call :CleanNETCore AspNetCore.MongoDB
 call :CleanNETCore AspNetCore.NHibernate
+call :CleanNET Owin.Dapper
+call :CleanNET Owin.EFCore
+call :CleanNET Owin.MongoDB
+call :CleanNET Owin.NHibernate
 timeout 5
 exit 0
 
+:CleanNET
+echo Clean %~1
+rmdir /s /q %~1\.vs
+rmdir /s /q %~1\packages
+rmdir /s /q %~1\__ProjectName__.AspNet\bin
+rmdir /s /q %~1\__ProjectName__.AspNet\obj
+rmdir /s /q %~1\src\__ProjectName__.Console\bin
+rmdir /s /q %~1\src\__ProjectName__.Console\obj
+rmdir /s /q %~1\src\__ProjectName__.Plugins\bin
+rmdir /s /q %~1\src\__ProjectName__.Plugins\obj
+exit /b 0
+
 :CleanNETCore
 echo Clean %~1
+rmdir /s /q %~1\.vs
 rmdir /s /q %~1\src\__ProjectName__.AspNetCore\bin
 rmdir /s /q %~1\src\__ProjectName__.AspNetCore\obj
 rmdir /s /q %~1\src\__ProjectName__.Console\bin
